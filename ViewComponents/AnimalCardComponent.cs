22764d16a644e4c0bf72427f3b130ddc077f2db5
Maksim Korolev <kimikirimusu@gmail.com>
2024-01-08 18:51:37 +0200

    catalog cards added

---
+++ ViewComponents/AnimalCardComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShopApplication.Models;
+
+namespace PetShopApplication.ViewComponents
+{
+    public class AnimalCardViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(Animal param)
+        {
+            return View("AnimalCard", param);
+        }
+    }
+}
