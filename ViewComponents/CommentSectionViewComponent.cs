8395aa84f61c89ecf59a691092e88239e6dda589
Maksim Korolev <kimikirimusu@gmail.com>
2024-01-16 16:48:05 +0200

    comment section added

---
+++ ViewComponents/CommentSectionViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShopApplication.ViewComponents
+{
+    public class CommentSectionViewComponent : ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult<IViewComponentResult>(View("_CommentSection"));
+        }
+    }
+}
