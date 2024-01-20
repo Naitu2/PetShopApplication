7098869969accd70dbfcb46e964c1b93997c4a72
Maksim Korolev <kimikirimusu@gmail.com>
2024-01-20 17:50:24 +0200

    animalupdate to animalform

--- ViewComponents/AnimalFormViewComponent.cs
+++ ViewComponents/AnimalFormViewComponent.cs
@@ -3,11 +3,11 @@ using PetShopApplication.Models;
 
 namespace PetShopApplication.ViewComponents
 {
-    public class AnimalUpdateViewComponent : ViewComponent
+    public class AnimalFormViewComponent : ViewComponent
     {
         public Task<IViewComponentResult> InvokeAsync(Animal updatedAnimal)
         {
-            return Task.FromResult<IViewComponentResult>(View("_AnimalUpdate", updatedAnimal));
+            return Task.FromResult<IViewComponentResult>(View("_AnimalForm", updatedAnimal));
         }
     }
 }
