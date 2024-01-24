632c028b00bf3c381cb6feb37e75c79742c67ea5
Maksim Korolev <kimikirimusu@gmail.com>
2024-01-24 16:17:40 +0200

    minor changes

--- Program.cs
+++ Program.cs
@@ -11,13 +11,6 @@ builder.Services.AddSingleton<IListViewModelService, ListViewModelService>();
 string connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"]!;
 builder.Services.AddDbContext<PetShopContext>(options => options.UseSqlServer(connectionString));
 
-/*builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-*/
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -29,8 +22,6 @@ using (var scope = app.Services.CreateScope())
     var environment = scope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
     FileService.ManageAnimalImages(environment);
 }
-
-/*app.UseSession();*/
 
 app.UseRouting();
 app.UseStaticFiles();
